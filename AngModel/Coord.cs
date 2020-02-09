b96959697303ac545ed81fd6251718066d6d4866
VVV <vvvofka@yandex.ru>
2020-02-09 23:51:13 +0300

    coord+

---
+++ AngModel/Coord.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AngModel {
+	class Coord {
+		public double x = 0;
+		public double y = 0;
+		public int ix { get { return Convert.ToInt32(x); } }
+		public int iy { get { return Convert.ToInt32(y); } }
+	} // *****************************************************************************
+}
--- AngModel/Model.cs
+++ AngModel/Model.cs
@@ -6,17 +6,26 @@ using System.Threading.Tasks;
 namespace AngModel {
 	class Model {
 		// All in milimeter
-		const double BallDiameter = 68.0;
-		const double TableWidth = 3550.0;
-		const double TableHeigh = 1775.0;
-		const double LoseCornerWidth = 72.0;
-		const double LoseCenterWidth = 82.0;
+		public const double BallDiameter = 68.0;
+		public const double TableWidth = 3550.0;
+		public const double TableHeigh = 1775.0;
+		public const double LoseCornerWidth = 72.0;
+		public const double LoseCenterWidth = 82.0;
 
 		Ball ballCue = new Ball(BallDiameter);
 		Ball ballTarget = new Ball(BallDiameter);
 		Loses loses = new Loses(LoseCornerWidth, LoseCenterWidth, TableWidth, TableHeigh);
+		public Lose activeLose { get; private set; }
 
+		public Model() {
+			setActiveLose(0);
+			setBallCue(2 * BallDiameter, 2 * BallDiameter);
+			setBallTarget(TableWidth / 4, TableHeigh / 2);
+		} // //////////////////////////////////////////////////////////////////////
+
+		public Lose setActiveLose(int index) { return activeLose = loses[index]; }
 		public void setBallCue(double X, double Y) { ballCue.set(X, Y); }
-		public void setballTarget(double X, double Y) { ballTarget.set(X, Y); }
+		public void setBallTarget(double X, double Y) { ballTarget.set(X, Y); }
+
 	} // ***************************************************************************
 }
